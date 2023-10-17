40a4274cf97fe92b5da7ab2a96650470e6ea407c
Juliana Guerreiro <126183294+juliana242@users.noreply.github.com>
2023-10-17 08:21:26 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,19 +1,13 @@
 # views.py
-
 from django.shortcuts import render
-from .forms import Formulariocontacto
+from .forms import ContactoForm
 
-def vista_contacto(request):
+def formulario_contacto(request):
     if request.method == 'POST':
-        form = Formulariocontacto(request.POST)
+        form = ContactoForm(request.POST)
         if form.is_valid():
-            # Procesar el formulario si es válido
-            nombre = form.cleaned_data['nombre']
-            correo = form.cleaned_data['correo']
-            mensaje = form.cleaned_data['mensaje']
-            # Puedes realizar acciones adicionales aquí, como enviar un correo electrónico o guardar en la base de datos
+            form.save()  # Guarda el objeto en la base de datos
     else:
-        # Mostrar un formulario en blanco si no se ha enviado
-        form = Formulariocontacto()
-
+        form = ContactoForm()
     return render(request, 'formulario_contacto.html', {'form': form})
+
